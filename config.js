82d64424380a07323379b90a368fc07ebdecaea0
whoisyashu <maheshwariy077@gmail.com>
2026-02-08 03:50:47 +0530

    Added Dotenv

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  TOKEN : "process.env.HIGHRISE_TOKEN ",
-  ROOM : "process.env.ROOM_ID",
+  TOKEN : "291e84c606bc23271e7655503b77373a449fa908c8c569f7fc4c47f71001c45f",
+  ROOM : "698326711d571f91cbf3d1e9",
 
   DANCE_ZONE: {
     minX: 1.5,
